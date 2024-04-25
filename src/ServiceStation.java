df95b2ba3a803ee373a43487e0d94ce6abebfc79
BFGprog <karpov0005@gmail.com>
2024-04-25 13:52:23 +0300

    Corrected remarks

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -1,33 +1,7 @@
 public interface  ServiceStation {
 
-    public default void check() {
-        if (this instanceof Car) {
-            System.out.println("Обслуживаем " + ((Car) this).getModelName());
-            for (int i = 0; i < ((Car) this).getWheelsCount(); i++) {
-                updateTyre();
-            }
-            checkEngine();
-        } else if (this instanceof Truck) {
-            System.out.println("Обслуживаем " + ((Truck) this).getModelName());
-            for (int i = 0; i < ((Truck) this).getWheelsCount(); i++) {
-                updateTyre();
-            }
-            checkEngine();
-            checkTrailer();
-        } else if (this instanceof Bicycle) {
-            System.out.println("Обслуживаем " + ((Bicycle) this).getModelName());
-            for (int i = 0; i < ((Bicycle) this).getWheelsCount(); i++) {
-                updateTyre();
-            }
-        }
-    }
-    private void updateTyre() {
-        System.out.println("Меняем покрышку");
-    }
-    private void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
-    private void checkTrailer() {
-        System.out.println("Проверяем прицеп");
-    }
+    void check();
+    void updateTyre();
+    void checkEngine();
+    void checkTrailer();
 }
--- src/Transport.java
+++ src/Transport.java
@@ -6,6 +6,21 @@ public abstract class Transport implements ServiceStation{
     private String modelName;
     private int wheelsCount;
 
+    @Override
+    public abstract void check();
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+    @Override
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+    @Override
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
 
     public String getModelName() {
         return modelName;
